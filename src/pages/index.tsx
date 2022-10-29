a2815b4e9558ee28c8a0cf3c4b820cb1d43ccc06
lfernanda2504 <luizafernandagarcia@gmail.com>
2022-10-29 12:57:56 -0500

    url banner update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage<HomeProps> = () => {
   return (
     <div className='font-medium text-xl font-poppins'>
       <ApplicationWrapper title="Home" description="Home of the store products">
-        <div className="grow flex flex-col justify-center items-center bg-[url('/images/banner.png')]">
+        <div className="grow flex flex-col justify-center items-center bg-[url('https://res.cloudinary.com/academia/image/upload/v1667065646/banner_jrc31n.png')]">
           <div className="grow bg-black w-full justify-center items-center flex opacity-70">
           <h1 className="text-5xl font-bold  text-white"> Bienvenid@ a Store Products </h1> 
           </div>
